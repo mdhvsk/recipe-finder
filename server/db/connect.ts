705d07dec9c698ccd7fc80395754cb2a79ffa1e2
madhav <madhavasok@gmail.com>
2024-01-10 19:01:20 -0800

    mysql connection made

---
+++ server/db/connect.ts
@@ -0,0 +1,14 @@
+var mysql = require('mysql');
+
+var pool = mysql.createPool({
+    connectionLimit: 10,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DB
+});
+
+pool.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+    if (error) throw error;
+    console.log('The solution is: ', results[0].solution);
+});
--- server/server.ts
+++ server/server.ts
@@ -1,20 +1,40 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const mysql = require('mysql2')
+const { OpenAI } = require("openai");
+const dotenv = require('dotenv')
+
+dotenv.config()
 
 const corsOptions = {
     origin: "http://localhost:3000"
 };
-app.use(express.json());
-app.use(cors(corsOptions));
 
-const { OpenAI } = require("openai");
 const configuration = {
     apiKey: process.env.OPENAI_API_KEY,
 };
 const openai = new OpenAI(configuration);
 
 const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(cors(corsOptions));
+
+
+
+var pool = mysql.createPool({
+    connectionLimit: 10,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DB
+});
+
+pool.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+    if (error) throw error;
+    console.log('The solution is: ', results[0].solution);
+});
 
 
 app.get('/trial', (req, res) => {
